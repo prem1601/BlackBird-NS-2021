d00227a3bb05d07e26620cb46b19e9b9075291e0
preetam chinde <preetamchinde@gmail.com>
2021-08-06 22:08:56 +0530

    added MergeSort of inversion count approach

--- Main/src/com/company/Assignments.java
+++ Main/src/com/company/Assignments.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Assignments {
 
     public static int dragonSlayer(int a, int b, int c, int d){
@@ -26,6 +28,60 @@ public class Assignments {
                 }
             }
         }
+        return count;
+    }
+
+    //Merge sort approach for inversionCount
+    public static long mergeAndCount(long[] arr, int l, int m, int r)
+    {
+
+        // Left subarray
+        long[] left = Arrays.copyOfRange(arr, l, m + 1);
+
+        // Right subarray
+        long[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l;
+        long swaps = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+        while (i < left.length)
+            arr[k++] = left[i++];
+        while (j < right.length)
+            arr[k++] = right[j++];
+        return swaps;
+    }
+
+    // Merge sort function
+    public static long  mergeSortAndCountInversion(long[] arr, int l,int r)
+    {
+
+        //keep count
+        long  count = 0;
+
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            // Total inversion count = left subarray count
+            // + right subarray count + merge count
+
+            // Left subarray count
+            count += mergeSortAndCountInversion(arr, l, m); //
+
+            // Right subarray count
+            count += mergeSortAndCountInversion(arr, m + 1, r);
+
+            // Merge count
+            count += mergeAndCount(arr, l, m, r);
+        }
+
         return count;
     }
 
