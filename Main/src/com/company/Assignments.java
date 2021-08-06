65f161246c6fb34cf5ff7e140843c1fbba2c45c1
preetam chinde <preetamchinde@gmail.com>
2021-08-06 21:35:54 +0530

    Changes in assignment of inversion count

--- Main/src/com/company/Assignments.java
+++ Main/src/com/company/Assignments.java
@@ -13,5 +13,20 @@ public class Assignments {
         return dragonSlayer(a-d, b, c-b, d);
     }
 
+    public static int inversionCount(int[] arr){
+        int count=0;
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]>arr[j]){
+                    count++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return count;
+    }
 
 }
--- Main/src/com/company/Main.java
+++ Main/src/com/company/Main.java
@@ -6,9 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter a number");
-//        int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
 //        ArrayList<Integer> list = PrimeNumbers.sieve(n);
 //
 //        for(int i=0; i<list.size(); i++)
@@ -23,6 +29,10 @@ public class Main {
 //            System.out.println("Natsu Won");
 //        }
 
-        d
+        System.out.println(Assignments.inversionCount(arr));
     }
+
+
+
+
 }
