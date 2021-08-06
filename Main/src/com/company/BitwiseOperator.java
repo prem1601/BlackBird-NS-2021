3398316ab4c1c48b60f37d06cf324041de426f53
preetam chinde <preetamchinde@gmail.com>
2021-08-06 22:44:04 +0530

    Bitwise Operator Examples added

---
+++ Main/src/com/company/BitwiseOperator.java
@@ -0,0 +1,96 @@
+package com.company;
+
+public class BitwiseOperator {
+
+    public static int bitwiseAnd(int n, int m) {
+        return n & m;
+    }
+
+    public static int bitwiseOr(int n, int m) {
+        return n | m;
+    }
+
+    public static int bitwiseNot(int n) {
+        return ~n;
+    }
+
+    public static int bitwiseXor(int n, int m) {
+        return n ^ m;
+    }
+
+    public static int rightShift(int num, int n) {
+        return num >> n;
+    }
+
+    public static int leftShift(int num, int n) {
+        return num << n;
+    }
+
+    public static int unsignedRightShift(int num, int n) {
+        return num >>> n;
+    }
+
+    public static void checkIfOddOrEven(int n) {
+        if ((n & 1) > 0) {
+            System.out.println("Odd Number");
+        } else {
+            System.out.println("Even Number");
+        }
+
+    }
+
+    public static void setNthBit(int x, int n) {
+        x |= 1 << n;
+        System.out.println(x);
+    }
+
+    public static void unsetNthBith(int x, int n) {
+        x &= ~(1 << n);
+        System.out.println(x);
+    }
+
+    public static void toggleNthBit(int x, int n) {
+        x ^= 1 << n;
+        System.out.println(x);
+    }
+
+    public static void checkIfPowerOfTwo(int n) {
+        if ((n & n - 1) == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+
+    public static void printFirstNNumbersWithTwoSetBits(int n) {
+        for(int x = 1; n > 0; ++x) {
+            for(int y = 0; y < x; ++y) {
+                System.out.print((1 << x) + (1 << y) + " ");
+                --n;
+                if (n == 0) {
+                    return;
+                }
+            }
+        }
+
+    }
+
+    public static void getSumOfBitDifferencesInAnArray(int[] arr) {
+        int res = 0;
+
+        for(int i = 0; i < 32; ++i) {
+            int count = 0;
+
+            for(int j = 0; j < arr.length; ++j) {
+                if ((arr[j] & 1 << i) == 0) {
+                    ++count;
+                }
+            }
+
+            res += count * (arr.length - count) * 2;
+        }
+
+        System.out.println(res);
+    }
+}
